c6617852bbb9044c78e4e1fcc04b1e6cdcd4e617
jgabt <johngabtolentino@gmail.com>
2022-08-13 03:06:51 +0800

    Side Tables, Footer

---
+++ frontend/src/user/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+export default function() {
+    return (
+        <>
+            <footer className="footer mt-auto">
+                <div className="container-fluid text-center py-3 o40">
+                    <hr style={{ width: '80%', }} className="ms-auto me-auto" />
+                    @ 2022 Live Tennis ELO Ratings. All rights reserved.
+                </div>
+            </footer>
+        </>
+    )
+}
